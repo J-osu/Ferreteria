f37492bb75fc9f2306333ecf394fc13092b8ec3f
J-osu <josuedelacr96@gmail.com>
2025-11-10 10:52:43 +0100

    Files modificated.(imports not used deleted.

--- backend-online-hardware/src/app.module.ts
+++ backend-online-hardware/src/app.module.ts
@@ -5,12 +5,16 @@ import { LoginModule } from './module/users/submodules/login/login.module';
 import { AdministrationModule } from './module/administration/administration.module';
 import { CatalogeModule } from './module/products/submodules/cataloge/cataloge.module';
 import { OrderProcessModule } from './module/products/submodules/order_process/order_process.module';
-import { OrderPUModule } from './module/users/submodules/profile_user/order_p_u.module';
-import { VisualPCModule } from './module/products/submodules/visual_p_c/visual_p_c.module';
 import { MainModule } from './module/main/main.module';
 
 @Module({
-  imports: [LoginModule, AdministrationModule, CatalogeModule, OrderProcessModule, OrderPUModule, VisualPCModule, MainModule],
+  imports: [
+    LoginModule,
+    AdministrationModule,
+    CatalogeModule,
+    OrderProcessModule,
+    MainModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
