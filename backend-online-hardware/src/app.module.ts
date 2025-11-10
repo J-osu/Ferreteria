905ffe0b570b4dcd98e9a06605edac6473c2842b
J-osu <josuedelacr96@gmail.com>
2025-11-10 13:52:37 +0100

    the modules already acepts requests.

--- backend-online-hardware/src/app.module.ts
+++ backend-online-hardware/src/app.module.ts
@@ -6,6 +6,14 @@ import { AdministrationModule } from './module/administration/administration.mod
 import { CatalogeModule } from './module/products/submodules/cataloge/cataloge.module';
 import { OrderProcessModule } from './module/products/submodules/order_process/order_process.module';
 import { MainModule } from './module/main/main.module';
+import { ProfileUserModule } from './module/users/submodules/profile_user/profile_user.module';
+import { UsersModule } from './module/users/users.module';
+import { ProductsModule } from './module/products/products.module';
+import { OrderPCModule } from './module/products/submodules/order_p_c/order_p_c.module';
+import { SearchFilterModule } from './module/products/submodules/order_p_c/submodules/search_filter/search_filter.module';
+import { AdministrationPCModule } from './module/administration/submodules/administration_p_c/administration_p_c.module';
+import { AdministrationUsersModule } from './module/administration/submodules/administration_users/administration_users.module';
+import { BackupModule } from './module/administration/submodules/backup/backup.module';
 
 @Module({
   imports: [
@@ -14,6 +22,14 @@ import { MainModule } from './module/main/main.module';
     CatalogeModule,
     OrderProcessModule,
     MainModule,
+    ProfileUserModule,
+    UsersModule,
+    ProductsModule,
+    OrderPCModule,
+    SearchFilterModule,
+    AdministrationPCModule,
+    AdministrationUsersModule,
+    BackupModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend-online-hardware/src/module/users/submodules/login/login.controller.ts
+++ backend-online-hardware/src/module/users/submodules/login/login.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { LoginService } from './login.service';
 
-@Controller('login')
+@Controller('/auth')
 export class LoginController {
   //With  this injected in the controller
   constructor(private readonly loginService: LoginService) {}
 
-  @Get()
-  getAll() {
-    return 'Login works!';
+  @Get('login')
+  getLogin() {
+    return 'Login works en funcionamiento!';
+  }
+  @Get('logout')
+  getLogout() {
+    return 'Logout works!';
+  }
+  @Get('register')
+  getRegister() {
+    return 'Register works!';
   }
 }
